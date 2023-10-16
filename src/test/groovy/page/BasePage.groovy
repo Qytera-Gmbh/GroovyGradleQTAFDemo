fda9c49b911badbd3117a1c91e85e27fbd8e2976
Matthias Eggert <matthias.eggert@qytera.de>
2023-10-16 13:36:18 +0200

    use qtaf config

--- src/test/groovy/page/BasePage.groovy
+++ src/test/groovy/page/BasePage.groovy
@@ -1,11 +1,12 @@
 package page
 
+import de.qytera.qtaf.core.config.ConfigurationFactory
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import utils.WebDriverSingleton
 
 abstract class BasePage {
-    private final static String BASE_URL = 'https://www.qytera.de'
+    private final static String BASE_URL = ConfigurationFactory.getInstance().getString('driver.remoteUrl')
 
     private WebDriver driver
 
--- src/test/groovy/utils/WebDriverSingleton.groovy
+++ src/test/groovy/utils/WebDriverSingleton.groovy
@@ -1,17 +1,42 @@
 package utils
 
+import de.qytera.qtaf.core.config.ConfigurationFactory
+import de.qytera.qtaf.core.config.entity.ConfigMap
+import de.qytera.qtaf.core.selenium.FirefoxDriver
 import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxOptions
+
+import java.time.Duration
 
 @Singleton
 class WebDriverSingleton {
     private WebDriver driver
+    ConfigMap configuration = ConfigurationFactory.getInstance()
 
     WebDriver getDriver() {
         if (!driver) {
             WebDriverManager.chromedriver().setup()
-            driver = new ChromeDriver()
+
+            switch(configuration.getString('driver.name')) {
+                case ['chrome', '']:
+                    ChromeOptions options = new ChromeOptions()
+                    if (configuration.getBoolean 'driver.headless') options.addArguments "--headless=new"
+                    driver = new ChromeDriver(options)
+                    break
+                case 'firefox':
+                    FirefoxOptions options = new FirefoxOptions()
+                    if (configuration.getBoolean 'driver.headless') options.addArguments "--headless=new"
+                    driver = new FirefoxDriver(options)
+                    break
+                default:
+                    break
+            }
+
+            def implicitTimeout = configuration.getInt('driver.implicitWaitTimeout')
+            if (implicitTimeout) driver.manage().timeouts().implicitlyWait Duration.ofSeconds(implicitTimeout)
         }
 
         driver
@@ -19,8 +44,10 @@ class WebDriverSingleton {
 
     void quitDriver() {
         if (driver) {
-            driver.quit()
-            driver = null
+            if (configuration.getBoolean 'driver.quitAfterTesting') {
+                driver.quit()
+                driver = null
+            }
         }
     }
 }
