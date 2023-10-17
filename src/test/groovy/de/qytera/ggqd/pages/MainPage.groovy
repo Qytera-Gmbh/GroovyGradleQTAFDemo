2b90786657d916526a57294cbb4a2d4aea5b825d
Matthias Eggert <matthias.eggert@qytera.de>
2023-10-17 13:51:42 +0200

    withPage()

--- src/test/groovy/de/qytera/ggqd/pages/MainPage.groovy
+++ src/test/groovy/de/qytera/ggqd/pages/MainPage.groovy
@@ -1,7 +1,14 @@
 package de.qytera.ggqd.pages
 
-class MainPage extends APage {
+import de.qytera.qtaf.core.guice.annotations.Step
+
+class MainPage extends APageWithMainMenu {
     {
         path = "/"
+    }
+
+    @Step(name = 'Click consulting button', description = 'Click consulting button')
+    void clickConsultingButton() {
+        'Beratungstermin vereinbaren'.click()
     }
 }
--- src/test/groovy/de/qytera/ggqd/tests/MainPageTest.groovy
+++ src/test/groovy/de/qytera/ggqd/tests/MainPageTest.groovy
@@ -8,11 +8,21 @@ import org.testng.annotations.Test
 
 @TestFeature
 class MainPageTest extends ATest {
-    @Test(testName = "GGQD-001", description = "test loading of main page with correct headline")
+    @Test(testName = "GGQD-Main-001", description = "test loading of main page with correct headline")
     void "test loading of main page with correct headline"() {
         withPage(load(MainPage)) {
             navigateToPage()
             checkCurrentUrl '/'
+            checkHeadline 'Die Experten in Continuous Testing und Testautomatisierung'
+        }
+    }
+
+    @Test(testName = "GGQD-Main-002", description = "test click on consulting button leads to appointment page")
+    void "test click on consulting button leads to appointment page"() {
+        withPage(load(MainPage)) {
+            navigateToPage()
+            clickConsultingButton()
+            checkCurrentUrl('/telefon-termin')
             checkHeadline 'Die Experten in Continuous Testing und Testautomatisierung'
         }
     }
